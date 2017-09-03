12e226d49a96d3dcbd91621f4b938181ee3777a3
James Regis <regis@lix.polytechnique.fr>
2017-09-03 19:03:01 -0400

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='python-demo',
+      version='1.0',
+      description='OpenShift App',
+      author='James Regis',
+      author_email='jreg2k@gmail.com',
+      url='https://www.python.org/community/sigs/current/distutils-sig',
+      install_requires=['Flask>=0.7.2', 'MarkupSafe'],
+      )
+
